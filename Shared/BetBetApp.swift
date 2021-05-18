c44f343cbe3e272ac605dc47e616bed81d717030
evanward1 <evanward@umich.edu>
2021-05-17 20:08:43 -0400

    Initial commit

---
+++ Shared/BetBetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetBetApp.swift
+//  Shared
+//
+//  Created by Evan Ward on 5/17/21.
+//
+
+import SwiftUI
+
+@main
+struct BetBetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
